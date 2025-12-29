2e30539ba2813a578958581d82c86133ac31ee53
cuongtq <cuongtqse180377@fpt.edu.vn>
2025-12-29 11:00:30 +0700

    init commit

---
+++ src/main/java/com/exe/unihome/UnihomeApplication.java
@@ -0,0 +1,13 @@
+package com.exe.unihome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnihomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnihomeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exe/unihome/UnihomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exe.unihome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnihomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
